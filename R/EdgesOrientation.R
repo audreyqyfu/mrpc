5bfde826236b87343f0b47c8cf346d142d809ad2
badsha <mdbadsha@uidaho.edu>
2017-04-24 12:23:07 -0700

    Condition if no edges with variants Remove previous repeat function

--- R/EdgesOrientation.R
+++ R/EdgesOrientation.R
@@ -51,13 +51,30 @@ EdgesOrientation<-function (gInput,NQ=NQ,verbose = FALSE)
     }
   }
   
+  #Condition for no edges with variants (in step 1) then MRPC is not applicable
+  if(length(edgesWithVFirst)==0 & length(edgesWithVSecond)==0 & length(edgesWithBothVs)==0){
+    stop("Error:no edges with variants, so MRPC is not applicable")
+  }
+  #OR we can use the following condition
+  #Start
+  if (all(tarmat == 0))
+  {
+    repeat {
+      old_tarmat <- tarmat
+      if (all(tarmat == old_tarmat))
+        break
+    }
+  }
+  #End
+  
   #Step-2
   #Start to orient v-structures
   cat("\n V-structures are as follows :\n")
   # extract edges involving at least one gene node
   #edgesWithGs <- edges[which (edges[,1]>NQ | edges[,2]>NQ), ]
   #ind <- edgesWithGs
-  
+  if (any(tarmat == 1)) #if at least one edge directed already made so far
+  {
   ind <- which(g1 == 1, arr.ind = TRUE)  #Pullout the all relation in adjacency matrix from undirected graph
   for (i in seq_len(nrow(ind))) {
     x <- ind[i, 1]
@@ -73,132 +90,123 @@ EdgesOrientation<-function (gInput,NQ=NQ,verbose = FALSE)
           cat("\n", V[x], "->", V[y], "<-", V[z], "\n")  #Printout the v-structures
         }
         tarmat[x, y] <- tarmat[z, y] <- 1
-        #tarmat[y, x] <- tarmat[y, z] <- 0
       }
     }
+  }
   }
   #End to orient v-structures
 
-  #Step-3 Orient remaining edges, weather MR is applicable or not based on the following process:
-      #Pullout edges to extract parent and child/gene nodes (goal to make a triplet)
-      #extract edges with involving parent and child/gene nodes
-      #ignore the nodes that already have direction
-      #form a triplet
-      #determine the direction using test results from part 1 based on MR
-      #Repeat step 3 until all undirected edges to directed
+  #Step-3
+       #Orient remaining edges, weather MR is applicable or not based on the following process:
+       #Pullout all edges from g1 matrix and define 1st and 2nd node (goal to make a triplet for the remaining edges)
+       #extract all edges with involving those 1st and 2nd nodes (from row and column of g)
+       #ignore the nodes that already have direction
+       #form a triplet
+       #determine the direction using test results from part 1 based on MR
+       #Repeat until all undirected edges to directed
+  
   #Start to orient remaining edges.
-
-  #Condition for no edges with any nodes
-  if (all(tarmat == 0))
+  if (any(tarmat == 1)) #if at least one edge directed already made so far
   {
-    repeat {
-      if (all(tarmat == 0))
-        break
-    }
-  }
-  
-  
-  repeat {
-
-    S1=which(tarmat==1,arr.ind = T) #Pullout edges to extract parent and child/gene nodes from tarmat
-    for (v in 1:nrow(S1)) {
-      S_pn=S1[v,1] #extract the parent nodes
-      S_gn=S1[v,2] #extract the gene/child nodes
-
-      S11_cp=which(g[,S_pn]==1,arr.ind = T) #pullout edges with parent nodes in column
-      S11_rp=which(g[S_pn,]==1,arr.ind = T) #pullout edges with parent nodes in row
-
-      S11_cg=which(g[,S_gn]==1,arr.ind = T) #pullout edges with gene/child nodes in column
-      S11_rg=which(g[S_gn,]==1,arr.ind = T) #pullout edges with gene/child nodes in row
-
-      S11=c(S11_cg,S11_rg,S11_cp,S11_rp)  #cobine all
-      S11<-S11[S11!= S_pn]                #ignore if parent nodes that already have direction
-      S11<-S11[S11!= S_gn]                #ignore if gene/child nodes that already have direction
-
-      if(length(S11)!=0) {
-        for (d2 in 1:length(S11)) {
-          #ignore if already have direction
-          if(tarmat[S_gn,S11[d2]]==1||tarmat[S11[d2],S_gn]==1) {
-            S111=integer(0)
-          } else {
-            S111=S11[d2]
+    edgeall=which(g1==1,arr.ind = T) #Pullout the all edges again from g1 matrix
+    for (u in 1:nrow(edgeall))
+    {
+      FirstNode=edgeall[u,1]  #extract the 1st node
+      SecondNode=edgeall[u,2] #extract the 2nd node
+      
+      edgesWithFirstNodeinCol=which(g[,FirstNode]==1,arr.ind = T) #pullout edges with 1st node in column from g
+      edgesWithFirstNodeinRow=which(g[FirstNode,]==1,arr.ind = T) #pullout edges with 1stt node in row from g
+      
+      edgesWithSecondNodeinCol=which(g[,SecondNode]==1,arr.ind = T) #pullout edges with 2nd nodes in column from g
+      edgesWithSecondNodeinRow=which(g[SecondNode,]==1,arr.ind = T) #pullout edges with gene/child nodes in row from g
+      
+      combineEdgesAll=c(edgesWithSecondNodeinCol,edgesWithSecondNodeinRow,edgesWithFirstNodeinCol,edgesWithFirstNodeinRow)  #cobine all
+      combineEdgesAll<-combineEdgesAll[combineEdgesAll!= FirstNode]   #ignore if 1st nodes
+      combineEdgesAll<-combineEdgesAll[combineEdgesAll!= SecondNode]  #ignore if 2nd nodes
+      
+      if(length(combineEdgesAll)!=0)
+      {
+        for (v in 1:length(combineEdgesAll))
+        {
+          if(tarmat[SecondNode,combineEdgesAll[v]]==1||tarmat[combineEdgesAll[v],SecondNode]==1)  #ignore if already have direction
+          {
+            edgesNew=integer(0)
           }
-
-          if(length(S111)>0) {
-            for (f1 in 1:length(S111)) {
-              y=S111[f1]
-              #for (f2 in 1:length(S_gn[])) {
-              x=S_gn
-              if(g[S_gn,S11[d2]]==0 & g[S11[d2],S_gn]==0) {
-                x=S_pn
+          else
+          {
+            edgesNew=combineEdgesAll[v]
+          }
+          
+          if(length(edgesNew)>0)
+          {
+            for (w in 1:length(edgesNew))
+            {
+              y=edgesNew[w]
+              x=SecondNode
+              if(g[SecondNode,combineEdgesAll[v]]==0 & g[combineEdgesAll[v],SecondNode]==0)
+              {
+                x=FirstNode
               }
-
+              
               z<-0
-
-            #Scenario-2 (gene is the first column of the triplets)
-            #If Gene (other than genetic variant) is the first column
-            if (x>NQ) {
-              ny=x  #New y
-              z=y
-              y=ny
-              d=tarmat[ ,c(y,z)]
-              x1=which(d== 1, arr.ind = TRUE) # Which node have relation with present nodes y and z
-              if (!empty(x1)) {
-                x=x1[1,1]  #New x is the row of x1
-              }
-
-              if (all(d==0)) {
-                d=tarmat[c(y,z), ]
+              
+      #Scenario-2 (gene is the first column of the triplets)
+              if (x>NQ) #If Gene (other than genetic variant) is the first column
+              {
+                ny=x  #New y
+                z=y
+                y=ny
+                d=tarmat[ ,c(y,z)]  #pullout the relation in column with y and z in tarmat
                 x1=which(d== 1, arr.ind = TRUE) # Which node have relation with present nodes y and z
-                if (!empty(x1)) {
-                  x=x1[1,2]   #New x is the column of x1
+                if (!empty(x1))
+                {
+                  x=x1[1,1]  #New x 
+                }
+                
+                if (all(d==0))  #If no relation found in column with y and z in tarmat
+                {
+                  d=tarmat[c(y,z), ]  #pullout the relation in row with y and z in tarmat
+                  x1=which(d== 1, arr.ind = TRUE) # Which node have relation with present nodes y and z
+                  if (!empty(x1))
+                  {
+                    x=x1[1,2]   #New x 
+                  }
+                  
+                }
+                
+              }
+              if ((x!=0 & y!=0 & z!=0) & (x!=y & y!=z & z!=x))
+              {
+                #Case-2: If,y and z are adjacent, x and z conditionally independent given y,
+                #then the edge direction will be y-->z
+                if (g1[y, z] == 1 & tarmat[y, z]!=1 & tarmat[z, y]!=1 & ((y %in% gInput@sepset[[x]][[z]]) || (y %in% gInput@sepset[[z]][[x]])))
+                {
+                  tarmat[y, z]  <- 1
+                  tarmat[z, y]  <- 0
+                }
+                #Case-3: If,y and z are adjacent, x and z conditionally dependent given y,
+                #then the edge direction will be z-->y.
+                if (g1[y, z] == 1 & tarmat[y, z]!=1 & tarmat[z, y]!=1 &!(y %in% gInput@sepset[[x]][[z]]) & !(y %in% gInput@sepset[[z]][[x]]))
+                {
+                  tarmat[z, y]  <- 1
+                  tarmat[y, z]  <- 0
+                }
+                #Case-4:If, y & z have relation and x & y conditionally dependent given z and x & z conditionally independent given y,
+                #then edge direction will be z<-->y
+                if (g1[y, z] == 1 & g1[x, z] == 1 & g1[x, y] == 1 & !(z %in% gInput@sepset[[x]][[y]]) &!(y %in% gInput@sepset[[x]][[z]]))
+                {
+                  tarmat[y, z]  <- 1
+                  tarmat[z, y]  <- 1
                 }
               }
-            }
-            if ((x!=0 & y!=0 & z!=0) & (x!=y & y!=z & z!=x)) {
-              #Case-2: If,y and z are adjacent, x and z conditionally independent given y,
-              #then the edge direction will be y-->z
-              if (g1[y, z] == 1 & (y %in% gInput@sepset[[x]][[z]]) || (y %in% gInput@sepset[[z]][[x]])) {
-                tarmat[y, z]  <- 1
-                tarmat[z, y]  <- 0
-              }
               
-              #Case-3: If,y and z are adjacent, x and z conditionally dependent given y,
-              #then the edge direction will be z-->y.
-              if (x!=y & y!=z & z!=x & g1[y, z] == 1 & tarmat[y, z]!=1 & !(y %in% gInput@sepset[[x]][[z]]) & !(y %in% gInput@sepset[[z]][[x]])) {
-                tarmat[z, y]  <- 1
-                tarmat[y, z]  <- 0
-              }
-              
-              #Case-4:If, y & z have relation and x & y conditionally dependent given z and x & z conditionally independent given y,
-              #then edge direction will be z<-->y
-              if (g1[y, z] == 1 & g1[x, z] == 1 & g1[x, y] == 1 & !(z %in% gInput@sepset[[x]][[y]])&!(y %in% gInput@sepset[[x]][[z]])) {
-                tarmat[y, z]  <- 1
-                tarmat[z, y]  <- 1
-              }
             }
           }
         }
-        }
       }
+      
     }
-  #End to orient remaining edges.
-
-#Repeat untill fulfill the given condition
-    tar1 <- tarmat
-    for (i in 1:nrow(tar1))
-      {
-      for (j in 1:ncol(tar1))
-        {
-        if(tar1[i,j]==tar1[j,i])
-        {
-          tar1[j,i]=0
-        }
-      }
-    }
-
-    if (length(which(tar1==1))==length(which(g==1))) #Condition for repeating
-      break
+    #End to orient remaining edges.
   }
   repeat {
     old_tarmat <- tarmat
