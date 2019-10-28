8163d23772ede52d6b4d5414c8885cb788b4a0e6
badsha <mdbadsha@uidaho.edu>
2019-10-28 16:05:08 -0700

    Updated

--- R/RobustCor.R
+++ R/RobustCor.R
@@ -6,14 +6,14 @@ RobustCor <- function(xx,Beta, plot=FALSE)
   if (any(is.na(xx))) #if any NA
   {
     set.seed(103)
-    xx=mice(xx,m=5,printFlag = F) #Multivariate Imputation by Chained Equations (MICE)
-    xx=complete(xx) #Creates imputed data sets
+    xx <- mice(xx,m=5,printFlag = F) #Multivariate Imputation by Chained Equations (MICE)
+    xx <- complete(xx) #Creates imputed data sets
   }
-  xx=as.matrix(xx)
+  xx <- as.matrix(xx)
   nn <- dim(xx)[1] # sample size
   mm <- dim(xx)[2]
   #####===============Initialization=================================
-  Mx<-NULL; Median<-NULL; Dist<-NULL; Data0<-NULL
+  Mx <- NULL; Median <- NULL; Dist <- NULL; Data0 <- NULL
   for (ii in 1:dim(xx)[2])
   {
       Median[ii] <- median(xx[,ii],na.rm = T)
@@ -24,7 +24,7 @@ RobustCor <- function(xx,Beta, plot=FALSE)
   #}
   for (jj in 1:dim(xx)[1])
   {
-    Dist[jj]<-sqrt(sum((xx[jj,]-Median)^2,na.rm = T))
+    Dist[jj] <- sqrt(sum((xx[jj,]-Median)^2,na.rm = T))
     }
   for (kk in 1:dim(xx)[1])
   {
@@ -37,9 +37,9 @@ RobustCor <- function(xx,Beta, plot=FALSE)
   #source("mpinv.r")
   Mo <-as.numeric(colMeans(Data0,na.rm = T))
   Vo <- as.matrix(cov(Data0,use = "pairwise.complete.obs"))
-  DiffTol = 0.005;
-  DiffNorm = +10000;
-  Iter = 0;
+  DiffTol <- 0.005;
+  DiffNorm <- +10000;
+  Iter <- 0;
   #cat("Test=",dim(Vo),"\n")
   ##Wx <- NULL
   while (DiffNorm > DiffTol)
@@ -47,7 +47,7 @@ RobustCor <- function(xx,Beta, plot=FALSE)
     Wx <- NULL
     for (j1 in 1:nn)
     { 
-    xx=replace(xx,is.na(xx),0)  
+    xx <- replace(xx,is.na(xx),0)  
     zo <- as.numeric(xx[j1,]-Mo)
     #zz <- bta*(t(zo)%*%solve(Vo)%*%zo)
     #if(det(t(Vo)%*%zo)<0.000005)
@@ -66,13 +66,13 @@ RobustCor <- function(xx,Beta, plot=FALSE)
     #norm2 <- sqrt(sum((M.new-Mo)^2))+sqrt(sum((V.new-Vo)^2))
     #DiffNorm <- norm2/norm1
     DiffNorm <- sqrt(sum((M.new-Mo)^2))/mm + sqrt(sum((V.new-Vo)^2))/mm
-    Mo = M.new
-    Vo = V.new
-    Iter = Iter + 1
+    Mo <- M.new
+    Vo <- V.new
+    Iter <- Iter + 1
     #cat("Iter,Diff=",c(Iter,DiffNorm),"\n")
   }
-  Cor.coef<-cov2cor(V.new)
-  Wt.weight <-Wx
+  Cor.coef <- cov2cor(V.new)
+  Wt.weight <- Wx
   if (plot) plot(Wt.weight)
   return(list(RR=Cor.coef, M=M.new, V=V.new, Wt=Wt.weight))
 }
