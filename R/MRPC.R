e8a693c916c959fd3d09e89f9ae62d87c7185e57
Audrey Fu <audreyqyfu@gmail.com>
2021-03-18 14:51:24 -0700

    Update description and code on discrete data
    Reverted back to specifying suffStat for discrete data outside MRPC.  Changed the description of 'suffStat' in the documentation to include that for discrete data.

--- R/MRPC.R
+++ R/MRPC.R
@@ -7,10 +7,6 @@ MRPC <- function (data, suffStat, GV, FDR = 0.05, alpha = 0.05, indepTest = c("g
 {
   cl <- match.call()
   
-  if(indepTest=="disCItest") {
-    suffStat <- list (dm = data, adaptDF = FALSE, n.min = 1000)
-  }
-  
   if (!missing(p))
     stopifnot(is.numeric(p), length(p <- as.integer(p)) ==
                 1, p >= 2)
